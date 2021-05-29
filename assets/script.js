1a8adc386cb2509fad20afa468fc04e46bbbcebd
Lola <mledun@hotmail.com>
2021-05-29 17:44:16 +0000

    added dom elements to js file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
-const sounds = document.querySelector(".sounds");
-let play = document.querySelector(".play-btn");
+let mediaPlayer = document.querySelector(".media-player");
+let playBtn = document.getElementsByClassName('play-btn');
+let backBtn = document.getElementsByClassName('back-btn');
+let forwardBtn = document.getElementsByClassName('forward-btn');
+
+let sounds = document.querySelector(".sounds");
+
+
+
+
 
 //play sounds
 
